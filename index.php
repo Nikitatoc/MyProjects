25b6ec1c12ace0cc60de1c72aaece86d45c1304c
Nikitatoc <94145387+Nikitatoc@users.noreply.github.com>
2022-07-17 22:44:13 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,105 @@
+<!doctype html>
+
+<p>
+    <?php
+    require "head.html"
+    ?>
+
+    <div class="div" ><h1>Enter your vehicle<br> details to receive a <br>free valuation.</h1>
+        <p>If your vehicle has less than 100,000 miles<br>
+            enter your vehicle and contact details and<br>
+            we will contact you with a cash valuation<br>
+            within 24 hours.</p></div>
+    <div class="div"><button id="reg">ENTER REG</button>
+        <label for="agr">
+            <p><input type="checkbox" name="agreement" id="agr"> </label> I confirm that the vehicle I am enquiring about has<br>
+        less than 100,000 miles.<br></p>
+        <button class="button">Get started</button></div>
+</div>
+<h2 class="centr" id="wniz">We Pay Cash For Cars</h2>
+<p class="centr" id="rasstoynie">CashForCars is the safe, easy and cost effective alternative to selling your car to a dealer or through<br>
+    private advertisement. We guarantee to buy your car whatever the make, age, model or condition. No<br>
+    hassle, no time wasters, no bouncing cheques and no fees. Sell your car in Belfast today!<br><br>
+    We operate from premises in the Belfast area and will buy cars throughout Northern Ireland.</p>
+<div id="paraller"><div id="wniz2">.</div><div id="paraller2"><h3 class="centr">How it works</h3>
+        <p class="centr">It's so easy to sell your car with CashForCars!</p></div>
+    <div id="div2"><div class="right"><h4>1. Enter Your Details</h4>
+            <p>It takes just a few minutes to complete a<br>
+                form providing us with details of your<br>
+                vehicle. Answer a few questions about your<br>
+                cars mileage, age, condition, etc and fill in<br>
+                your personal details.</p></div>
+        <div class="right"><h4>2. Receive a Price</h4>
+            <p>We will contact you within 24hrs. If you are<br>
+                satisfied with our valuation we will arrange<br>
+                for you to meet at our premises or for a<br>
+                of the CashForCars team to visit<br>
+                you.</p></div>
+        <div class="right"><h4>3. Sell Your Car</h4>
+            <p>Once you have met a member of our team<br>
+                the whole process should be concluded<br>
+                within 15–20 minutes. We will pay you by<br>
+                draft cheque or transfer the money<br>
+                securely by Faster Payments there and<br>
+                then, with no fees.</p></div></div></div>
+<img src="https://megastrazh24.com/wp-content/uploads/2021/04/2973216.jpg" alt="по заданию"
+     width=600px>
+<h2 class="centr">Why choose CashForCars to sell your car?</h2>
+<p class="centr">Advertising your car for sale privately can be a costly, not to<br>
+    mention frustrating process which is renowned for attracting time<br>
+    wasters.</p>
+<p class="centr">  Using CashForCars eliminates the hassle of strangers visiting your<br>
+    home, the worry associated with them taking a test drive uninsured<br>
+    and guarantees peace of mind by working with a reputable<br>
+    company.</p>
+<p class="centr"> By dealing directly with CashForCars you also eliminate the worry of<br>
+    cheques bouncing and unsatisfied customers returning to your<br>
+    door.</p>
+<p class="centr" id="wniz3"> CashForCars is the quick, easy and efficient way to turn your car<br>
+    into cash for whatever reason. If you are purchasing a replacement<br>
+    vehicle of a dealer, seling your car to CashForCars puts you in the<br>
+    driving seat and you will be able to negotiate a much better deal.</p>
+<div id="spisok"><div class="block1"><ul>
+            <li>Sell your car right away</li>
+            <li>Secure payment & release of ownership</li>
+            <li>Sales are final</li>
+            <li>Instant Payment</li>
+            <li>No Fees</li>
+            <li>No Strangers Visiting Your Home</li>
+            <li>No Time Wasters & Tyre Kickers</li>
+            <li>We'll take your car as it is</li>
+        </ul></div>
+    <div class="block2"><ul>
+            <li>We'll take your car as it is</li>
+            <li>Cash in hand and more of it</li>
+            <li>Transparent and fair appraisal</li>
+            <li>Know
+                the price upfront</li>
+            <li>Easy and convenient</li>
+            <li>Outstanding Finance Settled</li>
+            <li>No Bouncing Cheques</li>
+            <li>Avoid Uninsured Test Drives</li>
+        </ul></div></div>
+<div id="dls"><div class="block1" id="blue"><h3>Trade In</h3>
+        <p>Don't let dealers give you less at trade-in, take a look at<br>
+            why CashForCars is a great way to get more money for<br>
+            your car! There are many great reasons to choose us over<br>
+            a standard dealer trade-in!</p></div>
+    <div  class="block2" id="blue2"><h3>Sell On Your Own</h3>
+        <p>Selling a car on your own can be unpredictable and<br>
+            stressful, with CashForCars you can sell your car as soon<br>
+            as you're ready. Check out why we're better than selling it<br>
+            yourself!</p></div></div>
+<h2 id="left">What our customers<br> have been saying</h2>
+<div id="right"><p>"Can't recommend them enough.<br>
+        Their fast, friendly service was<br>
+        ideal, and they offered us a better<br>
+        price than other trade in sites..."</p></div>
+<div id="finalcolor"><h2 class="centr">Receive your free valuation now</h2>
+    <button class="button" id="final">get started</button></div>
+
+<?php
+require "footer.html"
+?>
+</body>
+</html>
